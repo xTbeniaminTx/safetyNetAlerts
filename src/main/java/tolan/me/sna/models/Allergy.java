8acacd0a890c2e10ca0191d1289102129134c279
xTbeniaminTx <beniamin.tolan@outlook.com>
2021-04-17 17:11:27 +0200

    entity allergy

--- src/main/java/tolan/me/sna/models/Allergy.java
+++ src/main/java/tolan/me/sna/models/Allergy.java
@@ -1,5 +1,40 @@
 package tolan.me.sna.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Allergy {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private long allergyId;
+
+  private String allergyName;
+
+  public Allergy() {
+  }
+
+  public Allergy(String allergyName) {
+    this.allergyName = allergyName;
+  }
+
+  public String getAllergyName() {
+    return allergyName;
+  }
+
+  public void setAllergyName(String allergyName) {
+    this.allergyName = allergyName;
+  }
+
+  @Override
+  public String toString() {
+    return "Allergy{" +
+        "allergyId=" + allergyId +
+        ", allergyName='" + allergyName + '\'' +
+        '}';
+  }
 
 }
