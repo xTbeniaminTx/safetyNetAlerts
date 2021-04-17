2e656af0d05625f9505f615acb6bbe49432f48da
xTbeniaminTx <beniamin.tolan@outlook.com>
2021-04-17 15:25:53 +0200

    initial files with spring boot initializer thymeleaf h2 jpa web devtools actuator

---
+++ src/test/java/tolan/me/sna/SafetyNetAlertsApplicationTests.java
@@ -0,0 +1,13 @@
+package tolan.me.sna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafetyNetAlertsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
